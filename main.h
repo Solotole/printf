51a028906f73cf55f83998fb242817e5c78c64f9
Bolt202 <botinani85@gmail.com>
2023-11-14 05:02:09 -0800

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-<<<<<<< HEAD
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -117,36 +116,3 @@ long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
 
-=======
-#include <stddef.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.hi>
-/**
- * struct specifiers - structure with 3 members
- * @ch: first member
- * @percent: second member
- * @string: third member
- * Description: structure that holds the various variables passed to _printf
- */
-//struct specifiers
-{
-	char ch;
-	char percent;
-	char *string;
-}s;
-int _printf(const char *format, ...);
-/**
- * print_buffer - prints contents of buffer if it exists
- * @beffer: array of characters
- * @buff_ind index at which to add next char, represents length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-if (*buff_ind > 0)
-	write(1, &buffer[0], *buff_ind);
-*buff_ind = 0;
-}
-
-#endif
->>>>>>> ca73bf39a0b99f05628fdec611924e1ea3731281
