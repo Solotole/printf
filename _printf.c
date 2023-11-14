3b23848a62d33e1bdfc8a2b75ae4e5c0cc2878e6
Bolt202 <botinani85@gmail.com>
2023-11-14 04:35:38 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,22 +1,14 @@
 #include "main.h"
-<<<<<<< HEAD
 
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
  * @format: format.
-=======
-/**
- * _printf - Printf function
- * @format: format.
- * Solomon and Bolton
->>>>>>> ca73bf39a0b99f05628fdec611924e1ea3731281
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
@@ -73,41 +65,3 @@ void print_buffer(char buffer[], int *buff_ind)
 	*buff_ind = 0;
 }
 
-=======
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-
-va_list list;
-char buffer[BUFF_SIZE];
-if (format == NULL)
-	return (-1);
-va_start(list, format);
-ifor (i = 0; format && format[i] != '\0'; i++)
-{
-	if (format[i] != '%')
-	{
-		buffer[buff_ind++] = format[i];
-		if (buff_ind == BUFF_SIZE)
-			print_buffer(buffer, &buff_ind);
-		printed_chars++;
-	}
-	else
-	{
-		print_buffer(buffer, &buff_ind);
-		flags = get_flags(format, &i);
-		width = get_width(format, &i, list);
-		precision = get_precision(format, &i, list);
-		size = get_size(format, &i);
-		++i;
-		printed = handle_print(format, &i, list, buffer,
-		flags, width, precision, size);
-		if (printed == -1)
-			return (-1);
-		printed_chars += printed;
-	}
-}
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
-}
->>>>>>> ca73bf39a0b99f05628fdec611924e1ea3731281
